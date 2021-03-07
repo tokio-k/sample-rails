e9093b8273daac40b326e9803d86d07f1151d88e
Toki <55911995+tokio-k@users.noreply.github.com>
2021-03-08 07:33:25 +0900

    sampleアプリ

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'f2aef34fbd05e94f0074ba7414ecff872a1271c1506465ef10705d1f499ba4bb699889464172d660f14685fb2ed5e899ff155fcc58c6772219f972e87bfd0ede'
