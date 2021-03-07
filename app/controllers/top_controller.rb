6550e4b67b80bd440f97a64e1de419ee28c43721
Toki <55911995+tokio-k@users.noreply.github.com>
2021-03-07 22:16:32 +0900

    sampleアプリ

--- app/controllers/top_controller.rb
+++
@@ -1,4 +0,0 @@
-class TopController < ActionController::Base
-    def index
-    end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_061242) do
+ActiveRecord::Schema.define(version: 2021_03_07_121434) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name", limit: 128, null: false
-    t.integer "age"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "username"
+    t.text "profile"
+    t.string "profile_image_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["name"], name: "index_users_on_name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "weight_histories", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "weight"
-    t.string "memo"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_weight_histories_on_user_id"
-  end
-
-  add_foreign_key "weight_histories", "users"
 end
